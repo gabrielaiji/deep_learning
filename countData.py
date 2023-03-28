61b4f95e98e40084d9117cfe38bfa4fd5aee3fc2
Rochaix--yamamoto Gabriel <gabriel.rochaixyamamoto@etu.inp-n7.fr>
2023-03-28 11:57:44 +0200

    loadimages

---
+++ countData.py
@@ -0,0 +1,43 @@
+import os, sys
+
+
+
+def countRecFiles(path):
+	dirs = os.listdir( path )
+
+	count = 0
+	for item in dirs:
+		itemPath = os.path.join(path, item)	
+
+		if os.path.isdir(itemPath):
+			count += countRecFiles(itemPath)
+		elif os.path.isfile(itemPath):
+			count +=1
+		else:
+			print("Unknown : " + path+item)
+
+	return count
+
+
+originPath = "./data/train/"
+dirs = os.listdir( originPath )
+
+#dirs = filter(os.path.isdir, os.listdir(path))
+
+countTotal = 0
+for item in dirs:
+	itemPath = os.path.join(originPath, item)
+
+	if os.path.isdir(itemPath):
+		#print(typePath)
+		countType = countRecFiles(itemPath)
+
+		#print(item +"\t: \t"+ str(countType))
+		#"Location: {0:20} Revision {1}".format(Location, Revision)
+
+		print("{0:11} : {1}".format(item, str(countType)))
+		countTotal += countType
+	else:
+		print("Not type : " + item)
+
+print("Total : "+ str(countTotal))
